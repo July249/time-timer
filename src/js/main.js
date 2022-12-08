4327e119ba27347cf40b19d34cc9af7235b9e347
jihyunglee <jhlee7125@gmail.com>
2022-12-08 22:05:55 +0900

    add: .timer-input 클래스명을 가진 input 요소들의 NodeList를 변수로 선언

--- src/js/main.js
+++ src/js/main.js
@@ -6,7 +6,18 @@ const $hourDisplay = $timer.querySelector(".hour-display .timer-counter");
 const $minuteDisplay = $timer.querySelector(".minute-display .timer-counter");
 const $secondDisplay = $timer.querySelector(".second-display .timer-counter");
 
+const $displays = [$hourDisplay, $minuteDisplay, $secondDisplay];
+
 /* timer input elements */
 const $hourInput = $timer.querySelector("#hour-input");
 const $minuteInput = $timer.querySelector("#minute-input");
 const $secondInput = $timer.querySelector("#second-input");
+
+const $inputs = $timer.querySelectorAll(".timer-input");
+
+/* button NodeList */
+const $controllerBtns = $buttons.querySelectorAll("button");
+
+function activeInput() {
+  $;
+}
