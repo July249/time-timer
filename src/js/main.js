5d16a5718d12061b38dcff7619fdfdfdcfe968f0
jihyunglee <jhlee7125@gmail.com>
2022-12-08 23:07:04 +0900

    add: countDown, milliSecondsUnitTransfer, timeDistributer 함수 구현 - 타이머 동작 기능과 밀리세컨즈 단위를 변환하는 기능, 변환된 시간을 각 요소에 전달하는 기능 추가

--- src/js/main.js
+++ src/js/main.js
@@ -19,6 +19,50 @@ const $inputs = $timer.querySelectorAll(".timer-input");
 const $controllerBtns = $buttons.querySelectorAll("button");
 
 /* functions */
+/* 타이머 카운트다운 기능 */
+let timeValue;
+
+/* 타이머 동작 함수 */
+function countDown(time, run) {
+  if (time > 1000 && run) {
+    /* 타이머 동작 */
+    timeDistributer(milliSecondsUnitTransfer(time - 1000));
+    timeValue = setTimeout(() => {
+      console.log(time - 1000);
+      countDown(time - 1000, true);
+    }, 1000);
+  } else if (time < 60 * 1000 && run) {
+    alertDisplay();
+  } else if (!run) {
+    /* 타이머 일시 정지 */
+    clearTimeout(timeValue);
+    /* timeValue 초기화 */
+    timeValue = 0;
+    return timeValue;
+  } else if (time === 0) {
+    alert("Time Over");
+  }
+}
+
+/* 밀리세컨드 단위를 시:분:초로 변환 */
+function milliSecondsUnitTransfer(time) {
+  const hour = parseInt(time / 3600000);
+  const min = parseInt((time - hour * 3600000) / 60000);
+  const sec = parseInt((time - hour * 3600000 - min * 60000) / 1000);
+
+  return { hour, min, sec };
+}
+
+/* 시간을 각 요소에 전달해주는 함수 */
+function timeDistributer({ hour, min, sec }) {
+  $hourDisplay.textContent = hour;
+  $minuteDisplay.textContent = min;
+  $secondDisplay.textContent = sec;
+
+  $hourInput.value = hour;
+  $minuteInput.value = min;
+  $secondInput.value = sec;
+}
 
 /* 요소 활성화 기능 */
 function activeInput() {
