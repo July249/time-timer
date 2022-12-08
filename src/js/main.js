7771003c3a50633769934cecdca9a61ca2a6a3b8
jihyunglee <jhlee7125@gmail.com>
2022-12-08 22:02:28 +0900

    add: 조작할 DOM 객체를 변수로 선언

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,12 @@
 const $timer = document.querySelector("#timer-display");
 const $buttons = document.querySelector("#btns-container");
+
+/* timer display elements */
+const $hourDisplay = $timer.querySelector(".hour-display .timer-counter");
+const $minuteDisplay = $timer.querySelector(".minute-display .timer-counter");
+const $secondDisplay = $timer.querySelector(".second-display .timer-counter");
+
+/* timer input elements */
+const $hourInput = $timer.querySelector("#hour-input");
+const $minuteInput = $timer.querySelector("#minute-input");
+const $secondInput = $timer.querySelector("#second-input");
